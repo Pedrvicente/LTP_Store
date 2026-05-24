285f968607cd1f09150989ba167e36600159595a
Pedrvicente <pedro12gil@gmail.com>
2026-05-24 11:04:07 +0100

    header refinement

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,17 +6,17 @@ export default function Header() {
 			<Link className="text-lg font-bold" to="/">
 				Online Store
 			</Link>
-			<nav className="flex gap-8 bg-white shadow-md rounded-full px-8 py-6 text-sm">
-				<Link to="/">Home</Link>
-				<Link to="#">Shop</Link>
-				<Link to="#">About</Link>
-				<Link to="#">Contacts</Link>
-				<Link to="#">Blog</Link>
+			<nav className="flex gap-8 bg-white shadow-md rounded-full px-8 py-4 text-sm">
+				<Link className="transition-colors hover:text-[#0046FE]" to="/">Home</Link>
+				<Link className="transition-colors hover:text-[#0046FE]" to="#">Shop</Link>
+				<Link className="transition-colors hover:text-[#0046FE]" to="#">About</Link>
+				<Link className="transition-colors hover:text-[#0046FE]" to="#">Contacts</Link>
+				<Link className="transition-colors hover:text-[#0046FE]" to="#">Blog</Link>
 			</nav>
 			<div className="flex gap-3">
-				<button className="rounded-full bg-[#0046FE] text-white px-6 py-2 hover:bg-[#0038CC] transition">Search</button>
-				<button className="rounded-full bg-[#0046FE] text-white px-6 py-2 hover:bg-[#0038CC] transition">Account</button>
-				<Link className="rounded-full bg-[#0046FE] text-white px-6 py-2 hover:bg-[#0038CC] transition" to="/cart">Cart</Link>
+				<button className="rounded-full bg-[#0046FE] text-sm text-white font-medium px-4 py-2 hover:bg-[#0038CC] transition">Search</button>
+				<button className="rounded-full bg-[#0046FE] text-sm text-white font-medium px-4 py-2 hover:bg-[#0038CC] transition">Account</button>
+				<Link className="rounded-full bg-[#0046FE] text-sm text-white dont-medium px-4 py-2 hover:bg-[#0038CC] transition" to="/cart">Cart</Link>
 			</div>
 		</header>
 	);
