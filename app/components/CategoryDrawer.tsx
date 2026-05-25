1e149e67b642a9dd7b503063330a823d15f4f14c
Pedrvicente <pedro12gil@gmail.com>
2026-05-25 19:54:48 +0100

    keep sorting between changes of categories

--- app/components/CategoryDrawer.tsx
+++ app/components/CategoryDrawer.tsx
@@ -1,6 +1,21 @@
 import { Link } from "react-router";
+import { useSearchParams } from "react-router";
 
 export default function CategoryDrawer({ categories }: { categories: string[]}) {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	function categoryURL(value: string) {
+		const params = new URLSearchParams(searchParams);
+		// if value is a category set value else delete category field
+		if (value) {
+			params.set("category", value);
+		} else {
+			params.delete("category");
+		}
+		params.set("page", "1");
+		return `?${params.toString()}`;
+	}
+
 	return (
 		<div className="fixed left-0 top-1/2 z-50 -translate-y-1/2 hidden md:block">
 			<div className="group flex -translate-x-56 items-center transition-transform duration-300 hover:translate-x-0">
@@ -8,10 +23,10 @@ export default function CategoryDrawer({ categories }: { categories: string[]}) 
 				<div className="w-56 bg-[#FFD66B] p-6 shadow-lg">
 					<h2 className="mb-3 font-light">Categories</h2>
 					<ul className="space-y-2 text-sm">
-						<li><Link to="/" className="hover:text-[#F5F5F5]">All</Link></li>
+						<li><Link to={categoryURL("")} className="hover:text-[#F5F5F5]">All</Link></li>
 						{categories.map((cat) => (
 						<li key={cat}>
-							<Link to={`?category=${cat}`} className="capitalize font-light transition-all duration-300 hover:text-[#F5F5F5]">
+							<Link to={categoryURL(cat)} className="capitalize font-light transition-all duration-300 hover:text-[#F5F5F5]">
 							{cat.replace("-", " ")}
 							</Link>
 						</li>
